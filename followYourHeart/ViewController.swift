13711a205327224e5f1d8641742207cf2b83102f
xinrui Bai <bxrbaixinrui@gmail.com>
2018-09-08 14:13:30 -0400

    Initial Commit

---
+++ followYourHeart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  followYourHeart
+//
+//  Created by Bai xinrui on 9/8/18.
+//  Copyright © 2018 Bai xinrui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
