404386176e8279606af6f657e1a691c9820a6790
xinrui Bai <bxrbaixinrui@gmail.com>
2018-09-09 14:28:11 -0400

    perform segue on main storyBoard, add UIColor extension

---
+++ followYourHeart/Controllers/TimeListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TimeListViewController.swift
+//  followYourHeart
+//
+//  Created by Bai xinrui on 9/9/18.
+//  Copyright © 2018 Bai xinrui. All rights reserved.
+//
+
+import UIKit
+
+class TimeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpNavigationBar()
+    }
+    
+    func setUpNavigationBar() {
+        self.navigationItem.title = "Time List"
+//        self.navigationItem.rightBarButtonItem
+    }
+
+}
--- followYourHeart/Controllers/ViewController.swift
+++ followYourHeart/Controllers/ViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBAction func startButtonAction(_ sender: Any) {
+        performSegue(withIdentifier: "GetMyTimeList", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
